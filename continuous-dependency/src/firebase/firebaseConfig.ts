3e8bc0b85d8d8f9da2b88bf58025e20494e9a20d
moshe <mosheb@tanit.cloud>
2020-02-10 23:03:55 +0200

    firebase initial setup

---
+++ continuous-dependency/src/firebase/firebaseConfig.ts
@@ -0,0 +1,11 @@
+ const firebaseConfig = {
+  apiKey: "AIzaSyB6GlbbVRc2vZJWEg7lvhd2TPxW9djiRE4",
+  authDomain: "continuous-dependency.firebaseapp.com",
+  databaseURL: "https://continuous-dependency.firebaseio.com",
+  projectId: "continuous-dependency",
+  storageBucket: "continuous-dependency.appspot.com",
+  messagingSenderId: "955861074551",
+  appId: "1:955861074551:web:dd9758154f29dd6086bc72",
+  measurementId: "G-2YCREQ8Z4M"
+};
+export default firebaseConfig;
