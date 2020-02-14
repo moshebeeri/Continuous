65effbd5ff24f749f2575d8e3f4dfd7bb16a9ac8
moshe <mosheb@tanit.cloud>
2020-02-14 21:12:56 +0200

    fire base setup and initial topbar

---
+++ continuous-dependency/src/components/Project/project.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Project = () => {
+
+  return <div>
+    this is a project with <button onClick={(event) => {console.log('clicked!!')}}>click</button> 
+  </div>
+
+}
+
+export default Project;
---
+++ continuous-dependency/src/components/TopBar/topbar.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, IconButton}  from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop:'15px',
+    width: '100%',
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  toolbar: {backgroundColor:'midnightblue'},
+  login:{verticalAlign: 'middle', marginTop: 'none'},
+  loginText: {fontSize: '60%', marginRight: '10px'}
+}));
+
+const TopBar = () => {
+  const classes = useStyles();
+  const githubLogin = () => {}
+  
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            PACKDEP - continous dependency
+          </Typography>
+          <Typography className={classes.login}>
+            <IconButton onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {console.log('login clicked')}} 
+                        edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <GitHubIcon/>
+            </IconButton>
+            <Typography className={classes.loginText}>Login With GitHub</Typography>
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default TopBar;
