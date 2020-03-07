31b2e3e0a7b4009edf4e7d72e823978a783052b9
moshe <mosheb@tanit.cloud>
2020-03-08 01:33:19 +0200

    branches and useEffect for github

--- packdep/src/App.js
+++ packdep/src/App.js
@@ -4,8 +4,27 @@ import './App.css'
 import 'typeface-roboto'
 import TopBar from './components/TopBar/topbar'
 import Project from './components/Project/project'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import firebase from 'firebase'
+import userActions from './redux/actions/userActions'
+import { useDispatch } from "react-redux"
 
 const App = () => {
+  const [user] = useAuthState(firebase.auth())
+  const db = firebase.firestore()
+  const dispatch = useDispatch()
+
+  if(user){
+    db.collection('github_users').where('uid', '==', user.uid).get().then( snapshot => {
+      try{
+        const githubUser = snapshot.docs[0].data()
+        dispatch(userActions.setGithubAccessToken(githubUser.token))
+      }catch(e){
+        console.log(e.message)
+      }
+    })
+}
+
   return (
     <div className="App">
       <TopBar/>
--- packdep/src/components/Project/project.js
+++ packdep/src/components/Project/project.js
@@ -1,64 +1,80 @@
-import React, {useState, useEffect} from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { Typography } from '@material-ui/core'
-import projectsActions from '../../redux/actions/projectsActions'
-import {getOctokit, getAuthenticatedOctokit} from '../../github'  
-import { useCollection } from 'react-firebase-hooks/firestore'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography } from "@material-ui/core";
+import projectsActions from "../../redux/actions/projectsActions";
+import { getOctokit, getAuthenticatedOctokit } from "../../github";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 const Project = () => {
-  const count = useSelector(state => state.projectsReducer.projectsCount)
-  const dispatch = useDispatch()
-  const accessToken = useSelector(state => state.githubUser.accessToken)
-  const githubUser = useSelector(state => state.githubUser.accessToken)
-  const [orgs, setOrgs] = useState([])
-  const [repos, setRepos] = useState([])
+  const count = useSelector(state => state.projectsReducer.projectsCount);
+  const dispatch = useDispatch();
+  const accessToken = useSelector(state => state.githubUser.accessToken);
+  const githubUser = useSelector(state => state.githubUser.accessToken);
+  const [orgs, setOrgs] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const [reposBranches, setReposBranches] = useState({});
 
-  function getUserGithubStatus(){
-    if(accessToken){
-      const octokit = getAuthenticatedOctokit(accessToken)
-      octokit.repos.list({per_page: 10, page: 1})
-      .then((response) => {
+  function getUserGithubStatus() {
+    if (accessToken) {
+      const octokit = getAuthenticatedOctokit(accessToken);
+      octokit.repos.list({ per_page: 10, page: 1 }).then(response => {
         // console.log(JSON.stringify(response))
-        setRepos(response.data.map(repo => repo.name))
+        setRepos(response.data.map(repo => repo.name));
         response.data.map(repo => {
-          octokit.repos.listBranches({
-            owner: repo.owner.login,
-            repo: repo.name
-          }).then((response) => {
-            console.log(JSON.stringify(response.data))
-            
-          }).catch((e) => {
-            console.log(e.message)
-          })
-          return true
-        })
-      })
-      octokit.orgs.listForAuthenticatedUser({per_page: 10, page: 1})
-      .then((response) => {
-        console.log(JSON.stringify(response))
-        setOrgs(response.data.map(org => org.login))
+          octokit.repos
+            .listBranches({
+              owner: repo.owner.login,
+              repo: repo.name
+            })
+            .then(response => {
+              reposBranches[repo.name] = response.data;
+              let newReposBranches = {...reposBranches }
+              newReposBranches[repo.name] = response.data
+              setReposBranches(newReposBranches);
+              console.log(JSON.stringify(reposBranches));
+            })
+            .catch(e => {
+              console.log(e.message);
+            });
+          return true;
+        });
       });
+      octokit.orgs
+        .listForAuthenticatedUser({ per_page: 10, page: 1 })
+        .then(response => {
+          console.log(JSON.stringify(response));
+          setOrgs(response.data.map(org => org.login));
+        });
     }
   }
 
-  return( <div>
-    <Typography>{count}</Typography>
-      This project is with <button onClick={(event) => {
-        dispatch(projectsActions.addProject())
-        getUserGithubStatus();
+  useEffect(() => {
+    if (accessToken) getUserGithubStatus();
+  }, [accessToken]);
 
-        console.log('clicked!!')
-      }}>Increase</button> 
-      {
-        accessToken? 
+  return (
+    <div>
+      <Typography>{count}</Typography>
+      This project is with{" "}
+      <button
+        onClick={event => {
+          dispatch(projectsActions.addProject());
+          console.log("clicked!!");
+        }}
+      >
+        Increase
+      </button>
+      {accessToken ? (
         <Typography>
           You have {repos.length} repos and you are member of {orgs.length} orgs
-          orgs {JSON.stringify(orgs)}
-        </Typography> :
+          orgs {JSON.stringify(orgs)} 
+          branches {JSON.stringify(Object.keys(reposBranches).length)}
+        </Typography>
+      ) : (
         <Typography>No repos for you</Typography>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
