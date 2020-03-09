edf7530940d9f7932466ef07f83c6fab63079ffa
moshe <mosheb@tanit.cloud>
2020-03-10 00:32:15 +0200

    repo select

--- packdep/src/components/Project/project.js
+++ packdep/src/components/Project/project.js
@@ -1,25 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Typography } from "@material-ui/core";
-import projectsActions from "../../redux/actions/projectsActions";
-import { getOctokit, getAuthenticatedOctokit } from "../../github";
-import { useCollection } from "react-firebase-hooks/firestore";
+import React, { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Typography } from "@material-ui/core"
+import projectsActions from "../../redux/actions/projectsActions"
+import {
+  Select,
+  Input,
+  MenuItem,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputLabel
+} from "@material-ui/core"
+import { getOctokit, getAuthenticatedOctokit } from "../../github"
+import { useCollection } from "react-firebase-hooks/firestore"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  }
+}))
 
 const Project = () => {
-  const count = useSelector(state => state.projectsReducer.projectsCount);
-  const dispatch = useDispatch();
-  const accessToken = useSelector(state => state.githubUser.accessToken);
-  const githubUser = useSelector(state => state.githubUser.accessToken);
-  const [orgs, setOrgs] = useState([]);
-  const [repos, setRepos] = useState([]);
-  const [reposBranches, setReposBranches] = useState({});
+  const count = useSelector(state => state.projectsReducer.projectsCount)
+  const dispatch = useDispatch()
+  const accessToken = useSelector(state => state.githubUser.accessToken)
+  const githubUser = useSelector(state => state.githubUser.accessToken)
+  const [orgs, setOrgs] = useState([])
+  const [repos, setRepos] = useState([])
+  const [reposBranches, setReposBranches] = useState({})
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const [repo, setRepo] = React.useState("")
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   function getUserGithubStatus() {
     if (accessToken) {
-      const octokit = getAuthenticatedOctokit(accessToken);
+      const octokit = getAuthenticatedOctokit(accessToken)
       octokit.repos.list({ per_page: 10, page: 1 }).then(response => {
         // console.log(JSON.stringify(response))
-        setRepos(response.data.map(repo => repo.name));
+        setRepos(response.data.map(repo => repo.name))
         response.data.map(repo => {
           octokit.repos
             .listBranches({
@@ -27,39 +62,86 @@ const Project = () => {
               repo: repo.name
             })
             .then(response => {
-              reposBranches[repo.name] = response.data;
-              let newReposBranches = {...reposBranches }
+              reposBranches[repo.name] = response.data
+              let newReposBranches = { ...reposBranches }
               newReposBranches[repo.name] = response.data
-              setReposBranches(newReposBranches);
-              console.log(`JSON.stringify(reposBranches)${JSON.stringify(reposBranches)}`);
+              setReposBranches(newReposBranches)
+              console.log(
+                `JSON.stringify(reposBranches)${JSON.stringify(reposBranches)}`
+              )
             })
             .catch(e => {
-              console.log(e.message);
-            });
-          return true;
-        });
-      });
+              console.log(e.message)
+            })
+          return true
+        })
+      })
       octokit.orgs
         .listForAuthenticatedUser({ per_page: 10, page: 1 })
         .then(response => {
-          console.log(JSON.stringify(response));
-          setOrgs(response.data.map(org => org.login));
-        });
+          console.log(JSON.stringify(response))
+          setOrgs(response.data.map(org => org.login))
+        })
     }
   }
 
   useEffect(() => {
-    if (accessToken) getUserGithubStatus();
-  }, [accessToken]);
+    if (accessToken) getUserGithubStatus()
+  }, [accessToken])
+  const renderRepoSelect = (repos) => {
+    const ret = repos.map((repo, i) => {
+      return <MenuItem id={i} value={repo}>{repo}</MenuItem>
+    })
+    console.log(`========>>> ${ret.length}`)
+    return ret
+  }
+
+  const handleChange = event => {
+    console.log(event.target.value)
+    setRepo(event.target.value || '')
+  }
 
   return (
     <div>
+      <Button onClick={handleClickOpen}>Open select dialog</Button>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Select Repo & Branch</DialogTitle>
+        <DialogContent>
+          <form className={classes.container}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="demo-dialog-native">Repo</InputLabel>
+              <Select
+              labelId="demo-dialog-select-label"
+              id="demo-dialog-select"
+              value={repo}
+              onChange={handleChange}
+              input={<Input />}
+            >
+                {renderRepoSelect(repos)}
+                </Select>
+            </FormControl>
+          </form>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Ok
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
       <Typography>{count}</Typography>
       This project is with{" "}
       <button
         onClick={event => {
-          dispatch(projectsActions.addProject());
-          console.log("clicked!!");
+          dispatch(projectsActions.addProject())
+          console.log("clicked!!")
         }}
       >
         Increase
@@ -67,14 +149,19 @@ const Project = () => {
       {accessToken ? (
         <Typography>
           You have {repos.length} repos and you are member of {orgs.length} orgs
-          orgs {JSON.stringify(orgs)} 
-          branches {JSON.stringify(Object.keys(reposBranches).map(key => reposBranches[key].length).reduce((prev, curr) => prev+curr, 0))}
+          orgs {JSON.stringify(orgs)}
+          branches{" "}
+          {JSON.stringify(
+            Object.keys(reposBranches)
+              .map(key => reposBranches[key].length)
+              .reduce((prev, curr) => prev + curr, 0)
+          )}
         </Typography>
       ) : (
         <Typography>No repos for you</Typography>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
